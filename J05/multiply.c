130866f5b8622aa5a4ce560c36047fb72fbc85fd
Elisabeth Brann <riversparks@wc133-dhcp-37.brynmawr.edu>
2025-04-08 16:17:02 -0400

    J05 done

--- J05/multiply.c
+++ J05/multiply.c
@@ -4,9 +4,27 @@
 #include <stdlib.h>
 #include <pthread.h>
 
-#define SIZE 100
+#define SIZE 4
 
 // your code here
+struct data {
+  int startIndex;
+  int endIndex;
+  int* matrix;
+  int* vector;
+  int* result;
+};
+
+void* product(void* data) {
+  struct data* info = data;
+  for (int i = info->startIndex; i < info->endIndex; i++) {
+    info->result[i] = 0;
+    for (int j = 0; j < SIZE; j++) {
+      info->result[i] = info->result[i] + (info->matrix[i * SIZE + j] * info->vector[j]);
+    }
+  }
+  return NULL;
+}
 
 int main(int argc, char *argv[]) {
   srand(time(0));
@@ -31,14 +49,29 @@ int main(int argc, char *argv[]) {
 
   // TODO: Implement your thread solution here
   printf("Test with 4 threads\n");
+  pthread_t *tid = (pthread_t *)malloc(sizeof(pthread_t) * 4);
+  int processesPerThread = SIZE/4;
 
+  struct data* info = (struct data*)malloc(sizeof(struct data) * 4);
+  for (int i = 0; i < 4; i++) {
+    info[i].startIndex = i * processesPerThread;
+    info[i].endIndex = (i + 1) * processesPerThread;
+    info[i].matrix = M;
+    info[i].vector = u;
+    info[i].result = result_threads;
+    pthread_create(&tid[i], NULL, product, (void*) &info[i]);
+  }
 
+  for (int i = 0; i < 4; i++) {
+    pthread_join(tid[i], NULL);
+  }
 
   int error = 0;
   for (int i = 0; i < SIZE; i++) {
     error += abs(result[i] - result_threads[i]);
   }
   printf("Error between ground truth and multithreaded version: %d\n", error);
-
+  free(tid);
+  free(info);
   return 0;
 }
