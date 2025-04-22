f0265f10f72b79044bf92aefe7a6a9f6cc8b9245
Elisabeth Brann <riversparks@wc133-dhcp-210.brynmawr.edu>
2025-04-22 11:23:40 -0400

    Assignment complete

--- A13/mylloc_list.c
+++ A13/mylloc_list.c
@@ -1,3 +1,10 @@
+/*----------------------------------------------
+* Author: Elisabeth Brann 
+* Date: 04/25/25
+* Description: Custom implementation of malloc and free based 
+* on the reading, with added functionality of tracking how much of
+* a chunk is in use.
+---------------------------------------------*/
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
@@ -10,11 +17,46 @@ struct chunk {
 struct chunk *flist = NULL;
 
 void *malloc (size_t size) {
-  // TODO: Implement malloc with a free list (See reading for details)
-  return NULL;
+  if (size <= 0) {
+    return NULL;
+  }
+  struct chunk *ptr = flist;
+  struct chunk *prev = NULL;
+  while (ptr != NULL) {
+    if (ptr->size >= size) {
+      if (prev != NULL) {
+        prev->next = ptr->next;
+      }
+      else {
+        flist = ptr->next;
+      }
+      ptr->used = size;
+      return (void*) (ptr + 1);
+    }
+    else {
+      prev = ptr;
+      ptr = ptr->next;
+    }
+  }
+  void *memory = sbrk(size + sizeof(struct chunk));
+  if (memory == (void*) - 1) {
+    return NULL;
+  }
+  else {
+    struct chunk *cnk = (struct chunk*) memory;
+    cnk->size = size;
+    cnk->used = size;
+    cnk->next = NULL;
+    return (void*) (cnk + 1);
+  }
 }
 
 void free(void *memory) {
-  // TODO: Implement malloc with a free list (See reading for details)
+  if (memory != NULL) {
+    struct chunk *cnk = (struct chunk*) ((struct chunk*) memory - 1);
+    cnk->next = flist;
+    cnk->used = 0;
+    flist = cnk;
+  }
   return;
 }
